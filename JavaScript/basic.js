2059c95473ce987f5d9fdc477cf01d8dbdfc804e
cashew <wcashew@hotmail.com>
2021-01-01 22:02:23 +0800

    add JavaScript

---
+++ JavaScript/basic.js
@@ -0,0 +1,10 @@
+
+/* var let 用于声明变量
+   var 声明的范围是函数作用域
+   let 声明的范围是块作用域
+   const 用于声明常量 声明时必须赋值
+   let const 是 ECMAScript 6 新增加 */
+var name = "XZ.PP";
+let bookNum = 0;
+const firstName = "Wang";
+
