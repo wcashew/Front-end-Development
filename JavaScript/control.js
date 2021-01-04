e45e3930f65d0b94ff73376f1411cb0e8bc2a241
cashew <wcashew@hotmail.com>
2021-01-04 21:56:16 +0800

    update control

---
+++ JavaScript/control.js
@@ -0,0 +1,11 @@
+
+var score = 80;
+if (score >= 90) {
+    console.log("优秀");
+} else if (score >= 80) {
+    console.log("良好");
+} else if (score >= 60) {
+    console.log("及格");
+} else {
+    console.log("不及格");
+}
